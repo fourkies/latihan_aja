e4b06691e12ee4efe08f21aad9be4d53053cd603
fourki <fourki2002@gmail.com>
2023-04-17 13:53:04 +0700

    web

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/dashboard-admin', function () {
+    return view('admin/dashboard_admin');
+});
+
+
+Route::controller(UserController::class)->name('user.')->group(function () {
+    Route::get('/user', 'getUser')->name('getUser');
+    Route::get('/tambah', 'tambahForm')->name('tambahForm');
 });
