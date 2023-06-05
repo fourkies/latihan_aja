ae6ea0e6f6f07a314f48341ed7890af97dd93a6d
fourki <fourki2002@gmail.com>
2023-06-05 15:02:30 +0700

    update login form

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,8 @@ use App\Http\Controllers\KehadiranController;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
+Route::get('/login', function () {
+    return view('login');
 });
 
 Route::get('/admin', function () {
