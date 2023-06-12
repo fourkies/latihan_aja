ad846067c27f0ca28f8ffdf6c7af90fb11a0505f
fourki <fourki2002@gmail.com>
2023-06-12 14:39:00 +0700

    update app components

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -182,7 +182,7 @@
 
 
     <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#">WELCOME</a>
+        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#">SIMPEG</a>
         <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse"
             data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false"
             aria-label="Toggle navigation">
@@ -192,9 +192,10 @@
             aria-label="Search">
         <div class="navbar-nav">
             <div class="nav-item text-nowrap">
-                @auth
-                    <a class="nav-link px-3" href="{{ route('logout') }}">Sign out {{ auth()->user()->name }}</a>
-                @endauth
+
+                <a class="nav-link px-3" href="{{ route('logout') }}">Sign out @auth{{ auth()->user()->name }}@endauth
+                </a>
+
             </div>
         </div>
     </header>
